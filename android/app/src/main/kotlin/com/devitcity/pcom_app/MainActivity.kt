f279879e59e0948bda90973e2df8d886799f763d
Muhammad Asad <asadrajputq518@gmail.com>
2025-06-12 23:17:51 +0100

    upgrade gradle

--- android/app/src/main/kotlin/com/devitcity/pcom_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.devitcity.pcom_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.devitcity.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
