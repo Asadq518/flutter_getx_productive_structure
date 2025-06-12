5d8f7c5d23fdf23538497ba800a1be99f5f61e36
Muhammad Asad <asadrajputq518@gmail.com>
2025-06-12 23:21:03 +0100

    open_settings fix

--- lib/utils/dialogs/internet_connectivity_dialog.dart
+++ lib/utils/dialogs/internet_connectivity_dialog.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
-import 'package:open_settings/open_settings.dart';
+import 'package:open_settings_plus/core/open_settings_plus.dart';
 
 import '../../app/core/resources/resources.dart';
 
@@ -22,21 +22,21 @@ Future<dynamic> internetConnectivityAlterDialog() {
         Text(
           'No Internet Connection Found!',
           style: TextStyle(
-              // color: primaryColor,
-              fontWeight: FontWeight.bold,
-              height: 1.5,
-              fontSize: 16.sp),
+            // color: primaryColor,
+            fontWeight: FontWeight.bold,
+            height: 1.5,
+            fontSize: 16.sp,
+          ),
         ),
         Text(
           'Please enable your internet',
           style: TextStyle(
-              // color: blackColor,
-              fontSize: 12.sp,
-              fontWeight: FontWeight.bold),
+            // color: blackColor,
+            fontSize: 12.sp,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-        const SizedBox(
-          height: 20,
-        ),
+        const SizedBox(height: 20),
         const Divider(
           height: 1,
           // color: blackColor,
@@ -45,32 +45,45 @@ Future<dynamic> internetConnectivityAlterDialog() {
           child: Row(
             children: [
               Expanded(
-                  child: TextButton(
-                      onPressed: () {
-                        Get.back();
-                        OpenSettings.openDataRoamingSetting();
-                      },
-                      child: Icon(
-                        Icons.sim_card,
-                        color: R.color.black,
-                      ))),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: VerticalDivider(
-                  thickness: .5,
-                  color: R.color.black,
+                child: TextButton(
+                  onPressed: () async {
+                    Get.back();
+                    switch (OpenSettingsPlus.shared) {
+                      case OpenSettingsPlusAndroid settings:
+                        await settings.dataRoaming();
+                        break;
+                      case OpenSettingsPlusIOS settings:
+                        await settings.cellular();
+                        break;
+                      default:
+                        throw Exception('Platform not supported');
+                    }
+                  },
+                  child: Icon(Icons.sim_card, color: R.color.black),
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: VerticalDivider(thickness: .5, color: R.color.black),
+              ),
               Expanded(
-                  child: TextButton(
-                      onPressed: () {
-                        Get.back();
-                        OpenSettings.openWIFISetting();
-                      },
-                      child: Icon(
-                        Icons.wifi,
-                        color: R.color.black,
-                      ))),
+                child: TextButton(
+                  onPressed: () async {
+                    Get.back();
+                    switch (OpenSettingsPlus.shared) {
+                      case OpenSettingsPlusAndroid settings:
+                        await settings.wifi();
+                        break;
+                      case OpenSettingsPlusIOS settings:
+                        await settings.wifi();
+                        break;
+                      default:
+                        throw Exception('Platform not supported');
+                    }
+                  },
+                  child: Icon(Icons.wifi, color: R.color.black),
+                ),
+              ),
             ],
           ),
         ),
